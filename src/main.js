cd8563a4e06b09efc94cf6e485b3e6e95d5b452b
YuhangXiong <yxiong4@ualberta.ca>
2019-08-20 16:02:38 -0600

    commit all files

--- src/main.js
+++ src/main.js
@@ -2,6 +2,33 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import firebase from 'firebase'
+
+Vue.config.productionTip = false
+
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCAOe9wlkKv1dQiEprZioGDN31mowue4zA",
+  authDomain: "realtime-chat-5e40f.firebaseapp.com",
+  databaseURL: "https://realtime-chat-5e40f.firebaseio.com",
+  projectId: "realtime-chat-5e40f",
+  storageBucket: "realtime-chat-5e40f.appspot.com",
+  messagingSenderId: "433407538546",
+  appId: "1:433407538546:web:9eaa7e43d8a711c2"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
+var db = firebase.firestore();
+
+window.db = db;
+
+db.settings({
+  timestampsInSnapshots:true
+});
+
+
 
 Vue.config.productionTip = false
 
--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Chat from './views/Chat.vue'
+import Login from './views/Login.vue'
 
 Vue.use(Router)
 
@@ -11,7 +13,12 @@ export default new Router({
     {
       path: '/',
       name: 'home',
-      component: Home
+      component: Chat
+    },
+    {
+      path:'/login',
+      name:'login',
+      component:Login
     },
     {
       path: '/about',
